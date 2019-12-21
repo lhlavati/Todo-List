403eb2fb8e09931d120b52c58d1d840640dc48ec
lhlavati <lhlavati@gmail.com>
2019-12-21 20:49:10 +0100

    Created table todo

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/student/Todo List/todo.db'
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    computer = db.Column(db.Boolean)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    return '<h1>{}</h1>'.format(request.form['todoitem'])
 
 if __name__ == '__main__':
     app.run(debug=True)
