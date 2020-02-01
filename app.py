40490898657f81d9df11685124d366edae3553c7
lhlavati <lhlavati@gmail.com>
2020-02-01 22:03:57 +0100

    Imported bootstrap and WTForms, created login.html and signup.html

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_fontawesome import FontAwesome
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/student/Todo List/todo.db'
 
 db = SQLAlchemy(app)
+Bootstrap(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +38,14 @@ def complete(id):
     todo.complete = True
     db.session.commit()
     return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
